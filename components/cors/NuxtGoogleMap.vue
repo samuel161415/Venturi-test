a754c05972669bb6a8444dc4174b0a1cee4adc9c
samuel161415 <samuelnegalign1@gmail.com>
2024-03-26 14:41:54 +0300

    adding 1 second for the icons to appear when the google map is loaded

--- components/cors/NuxtGoogleMap.vue
+++ components/cors/NuxtGoogleMap.vue
@@ -11,8 +11,10 @@
         :key="index"
         :options="{ position: location, anchorPoint: 'CENTER' }"
       >
-        <div style="text-align: center">
-          <div><p>{{ location_names[index] }}</p></div>
+        <div v-if="mapLoaded" style="text-align: center">
+          <div>
+            <p>{{ location_names[index] }}</p>
+          </div>
           <div class="w-5 h-10">
             <img
               src="../../static/logos/traffic.png"
@@ -24,6 +26,7 @@
     </MarkerCluster>
   </GoogleMap>
 </template>
+
 <script setup>
 import {
   GoogleMap,
@@ -31,19 +34,33 @@ import {
   MarkerCluster,
   CustomMarker,
 } from "vue3-google-map";
+import { ref, onMounted } from "vue";
 
 const runtimeConfig = useRuntimeConfig();
+const mapLoaded = ref(false);
+
+onMounted(() => {
+  // This is for simulating the icos appear after the google map is fully loaded
+  setTimeout(() => {
+    mapLoaded.value = true;
+  }, 1000);
+});
 
 const center = { lat: -31.56391, lng: 147.154312 };
-const location_names = ['location_x','location_y','location_z','location_a','location_b','location_c']
+const location_names = [
+  "location_x",
+  "location_y",
+  "location_z",
+  "location_a",
+  "location_b",
+  "location_c",
+];
 const locations = [
   { lat: -31.56391, lng: 147.154312 },
   { lat: -31.66605, lng: 147.254313 },
   { lat: -31.77393, lng: 147.354314 },
-  { lat: -31.44390, lng: 147.054310 },
+  { lat: -31.4439, lng: 147.05431 },
   { lat: -31.5989, lng: 147.194311 },
-  { lat: -31.36994, lng: 147.274310 },
-  
+  { lat: -31.36994, lng: 147.27431 },
 ];
-
 </script>
